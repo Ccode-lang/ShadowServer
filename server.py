6d3d08777b7b5f3cc45f2944d76bc730425c50d9
Ccode-lang <coop112097@gmail.com>
2025-01-04 10:20:05 -0700

    implement TCP

--- server.py
+++ server.py
@@ -1,17 +1,27 @@
 from socket import *
 import threading
 
-sock = socket(AF_INET, SOCK_DGRAM)
-sock.settimeout(1)
-sock.bind(("0.0.0.0", 4504))
 
-# Addr for client contact
-#clients = []
+# UDP socket info
+sockudp = socket(AF_INET, SOCK_DGRAM)
+sockudp.settimeout(1)
+sockudp.bind(("0.0.0.0", 4504))
+
+# TCP sock info
+socktcp = socket(AF_INET, SOCK_STREAM)
+socktcp.settimeout(1)
+socktcp.bind(("0.0.0.0", 4504))
 
 clientIDs = []
 
 # Vector3s
-clientspos = []
+clientspos = {}
+
+# is said client connected?
+client_connected = {}
+
+# Connected TCP clients tuples. (socket, id)
+TCP_clients = []
 
 idcounter = 0
 
@@ -30,16 +40,14 @@ class Vector3(object):
 
 import traceback
 
-def incoming_thread():
-    #global clients
+def incoming_udp_thread():
     global clientIDs
-    global idcounter
     global clientspos
 
 
     while True:
         try:
-            message, client_address = sock.recvfrom(256)
+            message, client_address = sockudp.recvfrom(256)
         except:
             continue
 
@@ -50,37 +58,101 @@ def incoming_thread():
         if msp[0] == "POSUPDT":
             # Server command code: POSUPDT:X:Y:Z:ID
             print("Got position update")
-            print(message)
-
-            if msp[4] == "NA":
-                print("Got new client")
-                #clients += [client_address[0]]
-                clientIDs += [idcounter]
-                msp[4] = str(idcounter)
-                idcounter += 1
-                sock.sendto(f"CLIENTID;{msp[4]}".encode(), client_address)
-            else:
-                sock.sendto("FILLER".encode(), client_address)
-
-            #sock.sendto(f"CLIENTID;{msp[4]}".encode(), client_address)
-            if not int(msp[4]) < len(clientspos):
-                clientspos += [Vector3(msp[1], msp[2], msp[3])]
-            else:
-                clientspos[int(msp[4])] = Vector3(msp[1], msp[2], msp[3])
+            clientspos[int(msp[4])] = Vector3(msp[1], msp[2], msp[3])
         elif msp[0] == "POSREQ":
             print(clientspos)
             # Client command code: POSUPDTCL;ID:X:Y:Z;(repeat as many times as needed)
             clmessage = "POSUPDTCL"
             for client in clientIDs:
-                #try:
-                clmessage += ";" + str(client) + ":" + clientspos[client].serialize()
-                #except:
-                #    pass
+                if client_connected[client]:
+                    clmessage += ";" + str(client) + ":" + clientspos[client].serialize()
             print(clmessage)
-            sock.sendto(clmessage.encode(), client_address)
-            
+            sockudp.sendto(clmessage.encode(), client_address)
 
-incoming_thread()
+def incoming_tcp_thread():
+    global idcounter
+    global TCP_clients
+    global clientIDs
+    socktcp.listen()
+    while True:
+        try:
+            connection, client_address = socktcp.accept()
+        except:
+            #traceback.print_exc()
+            continue
 
-sock.close()
+        print("Got new client")
+    
+     
+        client_connected[idcounter] = True
+        clientspos[idcounter] = Vector3(0, 0, 0)
+
+        thread = threading.Thread(target=user_tcp_thread, args=[connection, idcounter])
+        thread.daemon = True
+        thread.start()
+        clientIDs += [idcounter]
+
+        TCP_clients += [(connection, idcounter)]
+
+        idcounter += 1
+
+def user_tcp_thread(conn: socket, id: int):
+    # TCP Client Command code: CLIENTID;ID
+    conn.sendall(f"CLIENTID;{id}".encode())
+
+    while True:
+        data = conn.recv(1024).decode()
+
+        spldata = data.split(":")
+
+        if data == "DISCONN":
+            print("Got client disconnect")
+            client_connected[id] = False
+            conn.close()
+            for client in TCP_clients:
+                if client[1] == id:
+                    TCP_clients.remove(client)
+                    break
+            for id1 in clientIDs:
+                if id1 == id:
+                    clientIDs.remove(id)
+                    break
+            for id1 in clientspos.keys():
+                if id1 == id:
+                    del clientspos[id]
+                    break
+            send_disconnect(id)
+            break
+        elif spldata[0] == "CHAT":
+            send_chat(data.split(":", 1)[1])
+
+
+dislock = threading.Lock()
+def send_disconnect(id: int):
+    with dislock:
+        for client in TCP_clients:
+            client[0].sendall(f"DISCONNCL;{id}".encode())
+
+chatlock = threading.Lock()
+def send_chat(chmessage: str):
+    with chatlock:
+        for client in TCP_clients:
+            client[0].sendall(f"CHATCL;{chmessage}".encode())
+
+
+
+
+
+tcpThread = threading.Thread(target=incoming_tcp_thread)
+tcpThread.daemon = True
+tcpThread.start()
+
+try:
+    incoming_udp_thread()
+except KeyboardInterrupt:
+    # Quit gracefully
+    pass
+
+sockudp.close()
+socktcp.close()
 runThread = False
